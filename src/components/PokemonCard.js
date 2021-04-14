57568e11fc24627ac4c71f042dd9a2e594e06428
Alvaro Padilla <varoboss.ap@gmail.com>
2021-04-14 10:39:08 -0500

    Add bootstrap and sass

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,15 +4,21 @@ import PokemonType from './PokemonType'
 function PokemonCard({ imageUrl, name, types, id }) {
   return (
     <>
-      <p>Pokemon card</p>
-      <img
-        style={{ height: 200, width: 200, objectFit: 'cover' }}
-        src={imageUrl}
-        alt='Pokemon cover'
-      />
-      <p>{name}</p>
-      <p>{id}</p>
-      {types.map((type, i) => <PokemonType key={i} type={type} />)}
+      <div className='card'>
+        <img
+          style={{ height: 200, width: 200, objectFit: 'cover' }}
+          src={imageUrl}
+          alt='Pokemon cover'
+          className='card-img-top'
+        />
+        <div className='card-body'>
+          <h5 className='card-title'>{name}</h5>
+          <div className='card-text'>
+            <p>{id}</p>
+            {types.map((type, i) => <PokemonType key={i} type={type} />)}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
