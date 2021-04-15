92f10e4349357fe9aed4983caf0cc43bc8afee97
Alvaro Padilla <varoboss.ap@gmail.com>
2021-04-15 01:15:34 -0500

    Refactor shoping cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -15,11 +15,17 @@ function Cart () {
       <Card>
         <Card.Header>My shoping cart</Card.Header>
         <Card.Body>
-          <Card.Title>Pokemons</Card.Title>
+          <Card.Title>Pokemons to buy</Card.Title>
           <ListGroup className='cart-list'>
             {state.cartPokemons.map((pokemon, i) => <CartItem key={`${Date.now()}-${i}`} pokemon={pokemon} />)}
           </ListGroup>
-          <Button variant='primary' onClick={handlePurchasePokemons} className='mt-3'>Purchase</Button>
+          {state.cartCounter ? (
+            <>
+              <p className='mt-3'><span className='text-muted'>Total: </span>${state.cartTotal}</p>
+              <p className='mt-3'><span className='text-muted'>Quantity: </span>{state.cartCounter}</p>
+              <Button variant='primary' onClick={handlePurchasePokemons} className='mt-3'>Purchase</Button>
+            </>
+          ) : ''}
         </Card.Body>
       </Card>
     </>
